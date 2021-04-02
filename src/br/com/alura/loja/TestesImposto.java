45a20f08347fe8774c38ccf43948a31c5427306d
Renan Rodrigues Ramos <renan.ramos@inatel.br>
2021-04-01 23:08:38 -0300

    Padrao decorator para multiplos impostos

---
+++ src/br/com/alura/loja/TestesImposto.java
@@ -0,0 +1,19 @@
+package br.com.alura.loja;
+
+import java.math.BigDecimal;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.imposto.ISS;
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class TestesImposto {
+
+	public static void main(String[] args) {
+		Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
+		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+		
+		System.out.println(calculadora.calcular(orcamento, new ICMS(new ISS(null))));
+	}
+
+}
